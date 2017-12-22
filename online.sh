27578140e83787c0aae4a22864a85ec5f45fcf73
charles <xiaoaojianghuxq@126.com>
2017-12-22 10:55:16 +0800

    init

---
+++ online.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm -rf server/public/dist
+cd ./server/ && npm install  --registry=https://registry.npm.taobao.org
---
+++ server/config.js
@@ -0,0 +1,56 @@
+const CONF = {
+    port: '5757',
+    rootPathname: '',
+
+    // 微信小程序 App ID
+    appId: 'wx072faf1c8da06d99',
+
+    // 微信小程序 App Secret
+    appSecret: 'b9373bece6099c94676ed9d896fa23c3',
+
+    // 是否使用腾讯云代理登录小程序
+    useQcloudLogin: false,
+
+    /**
+     * MySQL 配置，用来存储 session 和用户信息
+     * 若使用了腾讯云微信小程序解决方案
+     * 开发环境下，MySQL 的初始密码为您的微信小程序 appid
+     */
+    mysql: {
+      host: 'rm-2zex0nenp88ilw31qpo.mysql.rds.aliyuncs.com',
+      port: 3306,
+      user: 'root',
+      db: 'cAuth',
+      pass: 'Welcome1',
+      char: 'utf8mb4'
+    },
+
+    cos: {
+        /**
+         * 区域
+         * 华北：cn-north
+         * 华东：cn-east
+         * 华南：cn-south
+         * 西南：cn-southwest
+         * 新加坡：sg
+         * @see https://www.qcloud.com/document/product/436/6224
+         */
+        region: 'cn-south',
+        // Bucket 名称
+        fileBucket: 'qcloudtest',
+        // 文件夹
+        uploadFolder: ''
+    },
+
+    // 微信登录态有效期
+    wxLoginExpires: 7200,
+    wxMessageToken: 'abcdefgh',
+    serverHost: 'edu.envol.vip',
+    tunnelServerUrl: '',
+    tunnelSignatureKey: '',
+    qcloudAppId: '',
+    qcloudSecretId: '',
+    qcloudSecretKey: ''
+}
+
+module.exports = CONF
